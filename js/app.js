d522f418836a9caa2543aa422d08fe0da4affe2f
Varun Grover <grover82vinni@gmail.com>
2021-03-02 21:44:06 +0530

    completed

---
+++ js/app.js
@@ -0,0 +1,94 @@
+// 1
+function show1(){
+    if(document.getElementById('ans1').style.display === 'none'){
+        // down
+        document.getElementById('up1').style.transform = 'rotate(180deg)';
+        
+        // show answer
+        document.getElementById('ans1').style.display = 'block';
+    }
+
+    else{
+        // up
+        document.getElementById('up1').style.transform = 'rotate(0deg)';
+
+        // hide answer
+        document.getElementById('ans1').style.display = 'none';
+    }
+}
+
+// 2
+function show2(){
+    if(document.getElementById('ans2').style.display === 'none'){
+        // down
+        document.getElementById('up2').style.transform = 'rotate(180deg)';
+        
+        // show answer
+        document.getElementById('ans2').style.display = 'block';
+    }
+
+    else{
+        // up
+        document.getElementById('up2').style.transform = 'rotate(0deg)';
+
+        // hide answer
+        document.getElementById('ans2').style.display = 'none';
+    }
+}
+
+// 3
+function show3(){
+    if(document.getElementById('ans3').style.display === 'none'){
+        // down
+        document.getElementById('up3').style.transform = 'rotate(180deg)';
+        
+        // show answer
+        document.getElementById('ans3').style.display = 'block';
+    }
+
+    else{
+        // up
+        document.getElementById('up3').style.transform = 'rotate(0deg)';
+
+        // hide answer
+        document.getElementById('ans3').style.display = 'none';
+    }
+}
+
+// 4
+function show4(){
+    if(document.getElementById('ans4').style.display === 'none'){
+        // down
+        document.getElementById('up4').style.transform = 'rotate(180deg)';
+        
+        // show answer
+        document.getElementById('ans4').style.display = 'block';
+    }
+
+    else{
+        // up
+        document.getElementById('up4').style.transform = 'rotate(0deg)';
+
+        // hide answer
+        document.getElementById('ans4').style.display = 'none';
+    }
+}
+
+// 5
+function show5(){
+    if(document.getElementById('ans5').style.display === 'none'){
+        // down
+        document.getElementById('up5').style.transform = 'rotate(180deg)';
+        
+        // show answer
+        document.getElementById('ans5').style.display = 'block';
+    }
+
+    else{
+        // up
+        document.getElementById('up5').style.transform = 'rotate(0deg)';
+
+        // hide answer
+        document.getElementById('ans5').style.display = 'none';
+    }
+}
